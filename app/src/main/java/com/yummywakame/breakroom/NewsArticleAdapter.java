0e90be512b23ef4c78d7136c5776ad3bc77ad719
Olivia Meiring <olivia@yummy-wakame.com>
2018-08-01 14:26:58 -0700

    Added @Nullable annotation as needed.

--- app/src/main/java/com/yummywakame/breakroom/NewsArticleAdapter.java
+++ app/src/main/java/com/yummywakame/breakroom/NewsArticleAdapter.java
@@ -18,6 +18,7 @@ package com.yummywakame.breakroom;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,7 +56,7 @@ public class NewsArticleAdapter extends ArrayAdapter<NewsArticle> {
      * in the list of articles.
      */
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         // Check if there is an existing list item view (called convertView) that we can reuse,
         // otherwise, if convertView is null, then inflate a new list item layout.
         View listItemView = convertView;
